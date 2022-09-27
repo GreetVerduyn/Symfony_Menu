6b10da6ec9df0cb42d855295a172a518418b9a56
Greet Verduyn <greetverduyn@proximus.be>
2022-09-27 10:06:41 +0200

    show all dishes

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\DishesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MenuController extends AbstractController
+{
+    #[Route('/menu', name: 'app_menu')]
+    public function menu(DishesRepository $dr): Response
+    {
+        $dishes = $dr->findAll();
+
+        return $this->render('menu/index.html.twig', [
+            'dishes' =>$dishes,
+        ]);
+    }
+}
